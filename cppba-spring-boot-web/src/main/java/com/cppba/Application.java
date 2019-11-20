cbc85059f8c0fe3a514e998f204ba4a4e4986880
leisurely2010 <33509636+leisurely2010@users.noreply.github.com>
2019-11-20 17:57:11 +0800

    Update Application.java

--- cppba-spring-boot-web/src/main/java/com/cppba/Application.java
+++ cppba-spring-boot-web/src/main/java/com/cppba/Application.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 // same as @Configuration @EnableAutoConfiguration @ComponentScan
 @SpringBootApplication
 //WebFilter,WebServlet,WebListener
+//扫描servlet注解
 @ServletComponentScan
 public class Application {
     public static void main(String[] args) {
